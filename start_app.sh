d3c45ddfbdda23c14fd3ec5e08e4590b3d35e9d9
BonjurG <hmgrem@yandex.ru>
2022-07-19 21:12:41 +0500

    fix redme

--- start_app.sh
+++ start_app.sh
@@ -3,6 +3,10 @@
 # install requirements
 pip install -r requirements.txt
 
+# start celery worker
+celery -A site_html_elements_getter worker --loglevel=INFO --detach
+
+
 # run server
 python manage.py runserver
 
